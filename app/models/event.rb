653511d2656f53d6606600f75646b8388788ef1b
Clement Bouakkaz <clementbouakkaz@gmail.com>
2018-12-11 17:55:07 +0000

    modifying event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,7 @@ class Event < ApplicationRecord
   has_many :eventguests
   has_many :users, through: :eventguests
   has_many :comments, as: :commentable
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
