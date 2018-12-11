c334f75b3a4ca7391005a001c5af5741101cadfa
Clement Bouakkaz <clementbouakkaz@gmail.com>
2018-12-11 16:00:32 +0000

    changed columns type in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_10_194626) do
+ActiveRecord::Schema.define(version: 2018_12_11_152441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,8 +40,8 @@ ActiveRecord::Schema.define(version: 2018_12_10_194626) do
     t.string "name"
     t.string "location"
     t.integer "radius"
-    t.string "longitude"
-    t.string "latitude"
+    t.float "longitude"
+    t.float "latitude"
     t.date "start_date"
     t.date "end_date"
     t.boolean "public"
