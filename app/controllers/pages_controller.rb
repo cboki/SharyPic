0348c7836bd6f12467128b364c95341aa3ba6165
Max Gutenkauf <gutenkauf.max@gmail.com>
2018-12-11 18:33:16 +0100

    Cloudinary Setup DONE + Camera page semi-working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :camera]
   def home
+  end
+  def camera
   end
 end
