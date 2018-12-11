2a313d7a0235437df9cd088cb155cb498d057b17
Clement Bouakkaz <clementbouakkaz@gmail.com>
2018-12-11 11:36:44 +0000

    renamed models with right snake case convention

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :eventguests
-  has_many :events, through: :eventguests
+  has_many :event_guests
+  has_many :events, through: :event_guests
   has_many :photos
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
