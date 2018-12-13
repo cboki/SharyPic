af30198885c576ca2b31bdbacd80ce3a3ae70d94
Clelia Sapriel <clelia_sapriel@yahoo.fr>
2018-12-13 19:15:54 +0000

    start

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   has_many :comments
   has_many :event_guests
+  has_many :photos
   has_many :users, through: :event_guests
   has_many :comments, as: :commentable
 
