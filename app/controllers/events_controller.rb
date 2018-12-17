902fb51d08f5ad080b59ba97dfa429842a205953
Clelia Sapriel <clelia_sapriel@yahoo.fr>
2018-12-17 10:29:50 +0000

    fix event new

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,7 +24,7 @@ class EventsController < ApplicationController
     @event.creator = current_user
     authorize @event
     if @event.save
-      redirect_to event_path(@event)
+      redirect_to event_path(@event), notice: "Your event has been successfully added."
     else
       render :new
     end
