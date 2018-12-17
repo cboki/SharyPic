4539a577abdc83cb642bf563a6fdfd780f974bb8
Benjamin Dunisch <bdunisch@googlemail.com>
2018-12-17 15:32:45 +0000

    likes are now working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,9 @@ import { getUserLocationOnClick } from '../location/getPosition';
 import 'flatpickr/dist/flatpickr.css';
 import { datePicker } from '../components/_datepicker';
 import { fileUploader } from '../fileuploader/fileUploader';
+import { like } from '../like/_like.js';
 
 getUserLocationOnClick();
 datePicker();
 fileUploader();
+like();
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # mount_uploader :photo, PhotoUploader
   has_many :event_guests
   has_many :events, through: :event_guests
+  has_many :likes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -11,4 +12,7 @@ class User < ApplicationRecord
     !event.in?(events)
   end
 
+  def is_liking?(photo)
+    likes.find_by(photo_id: photo.id).present?
+  end
 end
