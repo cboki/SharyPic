317d60ede1036487547230ff481037f82d9e6f76
Benjamin Dunisch <bdunisch@googlemail.com>
2018-12-17 17:02:20 +0000

    done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,10 +5,8 @@ import { algoliaSearchBar } from '../algoliaplaces/algoliasearch';
 import 'flatpickr/dist/flatpickr.css';
 import { datePicker } from '../components/_datepicker';
 import { fileUploader } from '../fileuploader/fileUploader';
-import { like } from '../like/_like.js';
 
 getUserLocationOnClick();
 datePicker();
 fileUploader();
-like();
 algoliaSearchBar();
