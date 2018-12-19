a1d27a0e059476b391883d02f10d0a1c52d7067f
Benjamin Dunisch <bdunisch@googlemail.com>
2018-12-19 16:50:59 +0000

    added Scope

--- config/initializers/google.rb
+++ config/initializers/google.rb
@@ -4,7 +4,7 @@ GOOGLE_APPLICATION_OPTIONS = {
   private_key_id: ENV["GOOGLE_APPLICATION_PRIVATE_KEY_ID"],
   private_key: ENV["GOOGLE_APPLICATION_PRIVATE_KEY"],
   client_email: ENV["GOOGLE_APPLICATION_CLIENT_EMAIL"],
-  scope: ["https://www.googleapis.com/auth/cloud-platform", "https://www.googleapis.com/auth/cloud-vision"]
+  scope: ENV["GOOGLE_APPLICATION_SCOPE"].split(",")
 }
 
 GOOGLE_APPLICATION_CREDENTIALS = Google::Auth::Credentials.new(GOOGLE_APPLICATION_OPTIONS)
