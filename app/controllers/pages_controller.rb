26189cd3601d6c7fad4acc6e6cf73e2a2148b95d
Clement Bouakkaz <clementbouakkaz@gmail.com>
2018-12-20 12:54:11 +0000

    without redirection if logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :camera]
   def home
     @disable_nav = true
-    redirect_to events_path unless current_user.nil?
   end
 
   def camera
